3869e31b11efd75287626da9dab458049182edd8
Dean Fox <deanfoxcd@gmail.com>
2024-11-13 21:49:29 -0500

    add initial commit + first draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [bill, setBill] = useState(0);
+  const [tip, setTip] = useState(0);
+  const [friendTip, setFriendTip] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BillInput bill={bill} setBill={setBill} />
+      <TipPercentage tip={tip} setTip={setTip}>
+        How did you like the service?
+      </TipPercentage>
+      <TipPercentage tip={friendTip} setTip={setFriendTip}>
+        How did your friend like the service?
+      </TipPercentage>
+      <Output bill={bill} tip={tip} friendTip={friendTip} />
+      <Reset setTip={setTip} setBill={setBill} />
     </div>
   );
 }
 
-export default App;
+const BillInput = ({ bill, setBill }) => {
+  return (
+    <div>
+      <span>How much was the bill?</span>
+      <input
+        type="text"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      />
+    </div>
+  );
+};
+
+const TipPercentage = ({ tip, setTip, children }) => {
+  const options = [
+    { desc: 'Dissatisfied (0%)', value: 0 },
+    { desc: 'It was okay (5%)', value: 0.05 },
+    { desc: 'It was good (10%)', value: 0.1 },
+    { desc: 'Absolutely amazing! (20%)', value: 0.2 },
+  ];
+
+  return (
+    <div>
+      <span>{children}</span>
+      <select value={tip} onChange={(e) => setTip(Number(e.target.value))}>
+        {options.map((option) => (
+          <option key={option.desc} value={option.value}>
+            {option.desc}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+const Output = ({ bill, tip, friendTip }) => {
+  const totalTip = (tip + friendTip) / 2;
+  const totalBill = bill + bill * totalTip;
+
+  return (
+    <h3>
+      You pay ${bill === 0 ? 0 : totalBill.toFixed(2)} (${bill} + $
+      {(bill * totalTip).toFixed(2)})
+    </h3>
+  );
+};
+
+const Reset = ({ setBill, setTip }) => {
+  const handleReset = (params) => {
+    setBill(0);
+    setTip(0);
+  };
+
+  return (
+    <div>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+};
